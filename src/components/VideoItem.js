7ca84eeec50aaec083dc1cf58578feaa5d6ec532
Scott <Scotthall1983@hotmail.com>
2020-03-26 13:48:28 +0000

    Added Semantic UI links for styling and added some Semantic custom styling from Semantic-Ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      (
-      <img alt="video result" src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title})
+    <div className="video-item item">
+      <img
+        className="ui image"
+        alt="video result"
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title})</div>
+      </div>
     </div>
   );
 };
