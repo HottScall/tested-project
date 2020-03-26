bf1d858e11003cfe828694b8641b3f2de1587852
Scott <Scotthall1983@hotmail.com>
2020-03-26 16:00:38 +0000

    Updated VideoDetail with an if statement which fixes an error around snippets. Added a videoSrc variable which contains the embedded link for youtube. Calls those in the return statement, added a few more divs and a new iframe tag which displays the video.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 const KEY = "AIzaSyAiK9DXMfQ6s88LZcaPc7ebUlh0_4leHe8";
 
@@ -22,13 +23,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log("From the app", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
