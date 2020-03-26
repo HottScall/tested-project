fd039a7fd2593b45861596f1863882dfea985b9d
Scott <Scotthall1983@hotmail.com>
2020-03-26 15:26:21 +0000

    App.js - Created a new method named onVideoSelect. Passed it as a prop to VideoList which passed it down to VideoItem. When a user clicks a VideoItem it invokes the call back.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import VideoList from "./VideoList";
 const KEY = "AIzaSyAiK9DXMfQ6s88LZcaPc7ebUlh0_4leHe8";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -21,11 +21,18 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log("From the app", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
