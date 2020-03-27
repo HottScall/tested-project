922b2b4beda8b3c298e7ed353aa6c8666393f9db
Scott <Scotthall1983@hotmail.com>
2020-03-27 18:18:45 +0000

    Added to additional testing which are failing that need to be fixed

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,16 +2,9 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("renders the component to the screen", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists).toBe(true);
+  });
 });
-//
-// describe("App component", () => {
-//   it("starts with a count of 0", () => {
-//     const wrapper = shallow(<App />);
-//     const text = wrapper.find("p").text();
-//     expect(text).toEqual("Count: 0");
-//   });
-// });
--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -17,14 +17,14 @@ describe("SearchBar", () => {
     expect(wrapper.find("input").props().value).toEqual("hello");
   });
 
-  // it("when the form is submitted the event is cancelled", () => {
-  //   const wrapper = shallow(<SearchBar />);
-  //   let prevented = false;
-  //   wrapper.find("form").simulate("submit", {
-  //     preventDefault: () => {
-  //       prevented = true;
-  //     }
-  //   });
-  //   expect(prevented).toBe(true);
-  // });
+  it("when the form is submitted the event is cancelled", () => {
+    const wrapper = shallow(<SearchBar />);
+    let prevented = false;
+    wrapper.find("form").simulate("submit", {
+      preventDefault: () => {
+        prevented = true;
+      }
+    });
+    expect(prevented).toBe(true);
+  });
 });
